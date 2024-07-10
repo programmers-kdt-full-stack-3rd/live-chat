486733bf6e3a9604ee7d56a51d956c67cfa9ab67
hojun3377 <hojun3377@gmail.com>
2024-07-10 13:02:07 +0900

    ✨ [feat] #2: 랜덤 번호 생성 구현

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
 
+import { generateRandomNumber } from "../utils/random";
+
 export const register = (req: Request, res: Response, next: NextFunction) => {
 	// TODO: 회원가입
 };
@@ -9,9 +11,26 @@ export const login = (req: Request, res: Response, next: NextFunction) => {
 	// TODO: 로그인
 };
 
-export const emailAuth = (req: Request, res: Response, next: NextFunction) => {
-	// TODO: 이메일 인증
-	res.status(StatusCodes.OK).json({ message: "성공적인 응답" });
+export const emailAuth = async (req: Request, res: Response) => {
+	const { email, create_at } = req.body;
+
+	try {
+		// 랜덤 번호 생성
+		const ranNum = generateRandomNumber(4);
+		console.log(ranNum);
+
+		// jwt 토큰 생성
+
+		// 매일 발송
+
+		// 응답
+		res.status(StatusCodes.OK).json({ message: "성공적인 응답" });
+	} catch (error) {
+		console.error(error);
+		res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+			message: "서버 에러",
+		});
+	}
 };
 
 export const emailAuthCheck = (
