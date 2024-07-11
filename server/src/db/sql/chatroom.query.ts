36d18a444dcbabd796564b93e07ffe5276ea5019
장 재 훈 <peng2412@naver.com>
2024-07-11 12:38:08 +0900

    ✨ [feat] #8,#9: Implement chatrooms management API
    실시간 채팅이 이루어지는 채팅방 기능 구현하기 위해 `controller`, `db/context,sql`, `routes`를 작성.
    그리고, API 종류는 다음과 같습니다.
    - 채팅방 생성 기능 (createChatRoom)
    - 채팅방 목록 조회 기능 (getChatrooms)
    - 채팅방 이름 수정 기능 (updateChatroom)
    - 채팅방 삭제 기능 (deleteChatroom)
    - 채팅방 친구 초대 기능 (inviteFriendToChatroom)
    - 사용자 채팅방 떠나기 기능 (leaveChatroom)
    
    auth와 관련된 req.user.id 처리가 필요합니다.

--- server/src/db/sql/chatroom.query.ts
+++ server/src/db/sql/chatroom.query.ts
@@ -1,1 +1,18 @@
-// chatroom 쿼리 문자열로 정의 및 export
+export const CREATE_CHATROOM = "INSERT INTO chatrooms (name) VALUES (?)";
+export const ADD_CHATROOM_MEMBER =
+	"INSERT INTO chatroom_members (chatroom_id, user_id) VALUES (?,?)";
+export const GET_CHATROOMS = `
+    SELECT c.chatroom_id, c.name
+    FROM chatroom_members cm
+    JOIN chatrooms c ON cm.chatroom_id = c.chatroom_id
+    WHERE cm.user_id = ?`;
+export const UPDATE_CHATROOM =
+	"UPDATE chatrooms SET name = ?, last_modified_date = CURRENT_TIMESTAMP WHERE chatroom_id = ?";
+export const DELETE_CHATROOM = "DELETE FROM chatrooms WHERE chatroom_id = ?";
+export const DELETE_CHATROOM_MEMBERS =
+	"DELETE FROM chatroom_members WHERE chatroom_id = ?";
+
+export const INVITED_FRIEND_TO_CHATROOM =
+	"INSERT INTO chatroom_members (chatroom_id, user_id) VALUES (?,?)";
+export const LEAVE_CHATROOM =
+	"DELETE FROM chatroom_members WHERE chatroom_id = ? AND user_id = ?";
