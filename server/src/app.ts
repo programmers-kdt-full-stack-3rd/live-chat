bfbfe242fd7ad40b43d0ed762f200163ad799ce7
장 재 훈 <peng2412@naver.com>
2024-07-12 15:29:22 +0900

    ✨ [feat] #10 : 메세지 socket.io 설정
    웹소켓을 이용하여 채팅 메세지의 실시간성을 보장하기 위해 socket.io 설정합니다.
    사용자가 메세지를 전송하면, broadcasting과 더불어 addMessages를 통해 메세지가 DB에 저장됩니다.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,16 @@
 import express from "express";
+import http from "http";
 import dotenv from "dotenv";
-
 import usersRouter from "./routes/users";
 import chatroomsRouter from "./routes/chatrooms";
+import { initializeSocket } from "./socket";
 
 dotenv.config();
 
 const app = express();
+const server = http.createServer(app);
+
+initializeSocket(server);
 
 /**
  * API 라우팅
