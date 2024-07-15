fc59c76e7edb5d43d43f486fa878968ed30f3e31
hojun3377 <hojun3377@gmail.com>
2024-07-15 19:29:10 +0900

    ✨ [feat] #1 : 비밀번호 해싱처리 및 DB access layer 연결 구현

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,9 +1,43 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 
-export const register = (req: Request, res: Response) => {
-	// TODO: 회원가입
+import { createNewUser } from "../services/user";
+import { IRequest } from "../types";
+import { BadRequestError } from "../errors";
+
+/**
+ * POST /api/users/register
+ */
+const register = async (req: Request, res: Response, next: NextFunction) => {
+	const { email, verified } = (req as IRequest).tokenDecodedInfo;
+	const { password, name } = req.body;
+
+	try {
+		// 인증 확인
+		if (!verified) {
+			throw new BadRequestError("이메일 인증이 이뤄지지 않았습니다.");
+		}
+
+		// 유저 정보 생성
+		await createNewUser(email, name, password);
+
+		// 쿠키 삭제
+		res.clearCookie("auth_token");
+
+		// 응답
+		res.status(StatusCodes.SEE_OTHER).json({
+			message: "리다이렉트 로그인 페이지 url",
+		});
+	} catch (error) {
+		next(error);
+	}
 };
 
-export const login = (req: Request, res: Response) => {
+/**
+ * POST /api/users/login
+ */
+const login = (req: Request, res: Response) => {
 	// TODO: 로그인
 };
+
+export { register, login };
