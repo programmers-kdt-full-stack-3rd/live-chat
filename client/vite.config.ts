f64cb6d56a2f44c4b3e425095783e6488ad90bd3
hojun3377 <hojun3377@gmail.com>
2024-07-21 17:58:51 +0900

    🚀 [chore] #23 : 종속성 추가 및 .env 관련 vite 설정
    - 종속성: axios, @types/axios, dotenv

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), vanillaExtractPlugin()],
+	define: {
+		"process.env": process.env,
+	},
 });
